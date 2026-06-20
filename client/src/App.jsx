d4817c74fd925673047468c733d0a073ff84f3d8
rammanojpatnaik <ram.patnaik86@gmail.com>
2026-06-21 00:25:28 +0530

    Fix cookie not being set

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
+import { useEffect } from 'react';
 import Cookies from 'js-cookie';
 import axios from 'axios';
 
 import {SESSION_COOKIE, SERVER_ADDRESS} from "./constants";
 
 export default function App() {
-    const cookieValue = Cookies.get(SESSION_COOKIE);
-    console.log({cookieValue})
-    if (cookieValue == null) {
-        axios.post(`${SERVER_ADDRESS}/sign_up`)
-    }
-    
+  const cookieValue = Cookies.get(SESSION_COOKIE);
+    useEffect(() => {
+        if (cookieValue == null) {
+            axios.post(`${SERVER_ADDRESS}/sign_up`, null, { withCredentials: true });
+        }
+    }, [cookieValue]);
+
     return (
         <main>
             <h1>Avalon</h1>
